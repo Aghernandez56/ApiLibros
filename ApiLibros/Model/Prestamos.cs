768872294cbe425bb376af29a7e3df0cf9df36f3
Aghernandez56 <AGHERNANDEZ@UNINTER.EDU.MX>
2025-05-22 17:23:54 -0600

    model

---
+++ ApiLibros/Model/Prestamos.cs
@@ -0,0 +1,16 @@
+﻿namespace ApiLibros.Model
+{
+    public class Prestamos
+    {
+		private int IdLibro {  get; set; }
+        public string? Titlo { get; set; }
+        public string? Autor { get; set; }
+        public string? ISBN { get; set; }
+        public int? EjemplaresTotales { get; set; }
+        public string? EjemplaresDisponibles { get; set; }
+
+
+
+
+    }
+}
