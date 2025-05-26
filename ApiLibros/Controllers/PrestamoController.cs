3ab8268f9a73a9f3744423b788d9a2900b32c1b2
AnGH56 <112748@alumnouninter.mx>
2025-05-26 01:32:48 -0600

    crud terminado, falta jwt

--- ApiLibros/Controllers/PrestamoController.cs
+++ ApiLibros/Controllers/PrestamoController.cs
@@ -67,5 +67,37 @@ namespace ApiLibros.Controllers
                 return StatusCode(500, $"Error interno: {ex.Message}");
             }
         }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Prestamos>>> CrearPrestamos(Prestamos prestamo)
+        {
+            try
+            {
+                var response = await _context.Database.ExecuteSqlRawAsync(
+                  "Exec InsertarPrestamo @p0, @p1, @p2, @p3",
+                   prestamo?.UsuarioId, prestamo?.LibroId, prestamo?.FechaDevolucion, prestamo?.Estado
+                );
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error interno: {ex.Message}");
+            }
+        }
+
+        [HttpDelete("{prestamoid}")]
+        public async Task<IActionResult> DeleteNovio( int prestamoid)
+        {
+            try
+            {
+                await _context.Database.ExecuteSqlRawAsync("Exec DeletePrestamo @p0", prestamoid);
+                return Ok($"Prestamo con ID {prestamoid} eliminado correctamente.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error interno: {ex.Message}");
+            }
+        }
     }
 }
